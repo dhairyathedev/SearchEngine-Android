b97dc97d19db36fb33f99f32671259e205ccf396
Dhairya  Shah <dtshah24022006@gmail.com>
2024-08-07 22:20:56 +0530

    add link opening

--- app/src/main/java/com/example/searchengine/SearchResultAdapter.java
+++ app/src/main/java/com/example/searchengine/SearchResultAdapter.java
@@ -1,5 +1,7 @@
 package com.example.searchengine;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,6 +33,11 @@ public class SearchResultAdapter extends RecyclerView.Adapter<SearchResultAdapte
         holder.titleTextView.setText(result.getTitle());
         holder.bodyTextView.setText(result.getBody());
         holder.urlTextView.setText(result.getUrl());
+
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(result.getUrl()));
+            v.getContext().startActivity(intent);
+        });
     }
 
     @Override
