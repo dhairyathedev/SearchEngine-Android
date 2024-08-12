605dfb413fbf28dda905533fcc77e195db99aec2
DishaPatel9854 <147984372+DishaPatel9854@users.noreply.github.com>
2024-08-12 10:54:52 +0530

    Add shimmer effect

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.facebook.shimmer:shimmer:0.5.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
